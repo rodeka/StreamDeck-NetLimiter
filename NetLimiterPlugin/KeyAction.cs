cb767497e29f9eaf39e5c373aa5ed4bd0e8f8960
rodeka <rodeka.core@ya.ru>
2024-05-23 04:44:24 +0300

    just copied from old local rep

---
+++ NetLimiterPlugin/KeyAction.cs
@@ -0,0 +1,174 @@
+﻿using BarRaider.SdTools;
+using NetLimiter.Service;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLimiterPlugin
+{
+    [PluginActionId("com.rodeka.nlplugin.blockports")]
+    public class KeyAction : KeypadBase
+    {
+        private class PluginSettings
+        {
+            public static PluginSettings CreateDefaultSettings()
+            {
+                PluginSettings instance = new PluginSettings();
+                instance.Name = String.Empty;
+                instance.AppPath = String.Empty;
+                instance.StartPort = 0;
+                instance.EndPort = 0;
+                instance.BPS = 0;
+                return instance;
+            }
+
+            private NLClient client = new NLClient();
+            private Filter filt;
+            private Rule rule;
+            private Filter filtModel;
+            private Rule ruleModel;
+            private bool ready = false;
+
+            public void CreateFilter()
+            {
+
+                if (this.AppPath == String.Empty || this.Name == String.Empty)
+                    return;
+                this.client.Connect();
+                this.filtModel = new Filter(this.Name);
+                this.filtModel.Functions.Add(new FFAppIdEqual(new AppId(this.AppPath)));
+                if (this.StartPort > 0 && this.EndPort > 0)
+                    this.filtModel.Functions.Add(new FFRemotePortInRange(new PortRangeFilterValue(this.StartPort, this.EndPort)));
+                this.ruleModel = new LimitRule(RuleDir.In, this.BPS);
+                this.filt = this.client.Filters.Find(x => x.Name == this.Name);
+                if (this.filt != null)
+                    this.client.RemoveFilter(this.filt);
+                this.filt = this.client.AddFilter(this.filtModel);
+                this.rule = this.client.AddRule(this.filtModel.Id, this.ruleModel);
+                this.ready = true;
+                this.Disable();
+            }
+
+            public void RemoveFilter()
+            {
+                if (!this.ready)
+                    return;
+
+                this.filt = this.client.Filters.Find(x => x.Name == this.Name);
+                if (this.filt != null)
+                    this.client.RemoveFilter(this.filt);
+            }
+
+            public bool IsEnabled() =>
+                    this.rule.IsEnabled;
+
+            public void Disable()
+            {
+                if (!this.ready)
+                    return;
+
+                this.rule.IsEnabled = false;
+                this.client.UpdateRule(this.rule);
+            }
+
+            public void Toggle()
+            {
+                if (!this.ready)
+                    return;
+
+                this.rule.IsEnabled = !this.rule.IsEnabled;
+                this.client.UpdateRule(this.rule);
+            }
+
+            public bool IsReady()
+            {
+                return this.ready;
+            }
+
+            [FilenameProperty]
+            [JsonProperty(PropertyName = "name")]
+            public string Name { get; set; }
+
+            [JsonProperty(PropertyName = "appPath")]
+            public string AppPath { get; set; }
+
+            [JsonProperty(PropertyName = "startPort")]
+            public ushort StartPort { get; set; }
+
+            [JsonProperty(PropertyName = "endPort")]
+            public ushort EndPort { get; set; }
+
+            [JsonProperty(PropertyName = "bps")]
+            public ushort BPS { get; set; }
+
+
+        }
+
+        #region Private Members
+
+        private PluginSettings settings;
+
+        #endregion
+        public KeyAction(SDConnection connection, InitialPayload payload) : base(connection, payload)
+        {
+            if (payload.Settings == null)
+            {
+                this.settings = PluginSettings.CreateDefaultSettings();
+                SaveSettings();
+            }
+            else
+            {
+                Connection.SetStateAsync((uint)0);
+                this.settings = payload.Settings.ToObject<PluginSettings>();
+                this.settings.CreateFilter();
+                if (this.settings.IsReady())
+                {
+                    Connection.ShowAlert();
+                }
+            }
+        }
+
+        public override void Dispose()
+        {
+            this.settings.RemoveFilter();
+            Logger.Instance.LogMessage(TracingLevel.INFO, $"Destructor called");
+        }
+
+        public override void KeyPressed(KeyPayload payload)
+        {
+            this.settings.Toggle();
+            Logger.Instance.LogMessage(TracingLevel.INFO, "Key Pressed");
+        }
+
+        public override void KeyReleased(KeyPayload payload) 
+        {
+            if (this.settings.IsEnabled())
+                Connection.ShowOk();
+        }
+
+        public override void OnTick() { }
+
+        public override void ReceivedSettings(ReceivedSettingsPayload payload)
+        {
+            Tools.AutoPopulateSettings(settings, payload.Settings);
+            SaveSettings();
+        }
+
+        public override void ReceivedGlobalSettings(ReceivedGlobalSettingsPayload payload) { }
+
+        #region Private Methods
+
+        private Task SaveSettings()
+        {
+            return Connection.SetSettingsAsync(JObject.FromObject(settings));
+        }
+
+        #endregion
+
+    }
+}
---
+++ NetLimiterPlugin/install.bat
@@ -0,0 +1,23 @@
+REM USAGE: Install.bat <DEBUG/RELEASE> <UUID>
+REM Example: Install.bat RELEASE com.barraider.spotify
+setlocal
+cd /d %~dp0
+cd bin
+cd %1
+
+REM *** MAKE SURE THE FOLLOWING VARIABLES ARE CORRECT ***
+REM (Distribution tool be downloaded from: https://docs.elgato.com/sdk/plugins/packaging )
+SET OUTPUT_DIR=%TEMP%
+SET DISTRIBUTION_TOOL="D:\Sources\StreamDeck-Netlimiter\NetLimiterPlugin\DistributionTool.exe"
+SET STREAM_DECK_FILE="C:\Program Files\Elgato\StreamDeck\StreamDeck.exe"
+SET STREAM_DECK_LOAD_TIMEOUT=7
+
+taskkill /f /im streamdeck.exe
+taskkill /f /im %2.exe
+timeout /t 2
+del %OUTPUT_DIR%\%2.streamDeckPlugin
+%DISTRIBUTION_TOOL% -b -i %2.sdPlugin -o %OUTPUT_DIR%
+rmdir %APPDATA%\Elgato\StreamDeck\Plugins\%2.sdPlugin /s /q
+START "" %STREAM_DECK_FILE%
+timeout /t %STREAM_DECK_LOAD_TIMEOUT%
+%OUTPUT_DIR%\%2.streamDeckPlugin
